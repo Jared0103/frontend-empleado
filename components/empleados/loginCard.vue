279ad39290af3c88c6fceb7b63cf675a9edbd10e
Jared Ochoa <josejared8ac@hotmail.com>
2024-08-26 22:23:20 -0600

    listo de clase del viernes

---
+++ components/empleados/loginCard.vue
@@ -0,0 +1,66 @@
+<template>
+  <v-card elevation="0" color="deep-orange-lighten-1" width="400" height="300" rounded>
+    <v-card-title>
+      <v-row align="center" justify="center">
+        <span style="color: orangered">
+          Bienvenido
+        </span>
+      </v-row>
+    </v-card-title>
+    <v-card-text class="mt-4 mb-4 pt-4 pb-4">
+      <div class="textFields">
+        <v-row style="width: 100%">
+          <v-text-field v-model="usuario" :rules="size" solo />
+        </v-row>
+        <v-row style="width: 100%">
+          <v-text-field v-model="password" :rules="size" type="password"  solo />
+        </v-row>
+      </div>
+    </v-card-text>
+    <v-card-text>
+      <v-row>
+        <v-text-field v-model="üsuario" />
+      </v-row>
+    </v-card-text>
+    <v-card-actions>
+      <v-row align="center" justify="center">
+        <v-btn elevation="0" color="deep-orange-lighten-5">
+          <span style="text-transform: none; color:#F4511E ;"> <!--me hace falta una aprte dcódigo aqui jajas-->
+            Login
+          </span>
+        </v-btn>
+      </v-row>
+    </v-card-actions>
+  </v-card>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      usuario: '',
+      password: '',
+      size: [
+        v => v.trim().length === 0 || 'No puede estar vacio'
+      ]
+    }
+  },
+  methods: {
+    loginBackend () {
+      console.log('@@@ variables', this.usuario, this.password)
+      const body = {
+        correo: this.usuario,
+        contrasena: this.password
+      }
+      this.$axios.post('/login', body)
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .textFields{
+    width: 100%;
+    margin: 10px;
+  }
+</style>
